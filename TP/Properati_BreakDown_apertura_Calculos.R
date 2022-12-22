e30d6b343136826f6e450f8c640f1017020a6cd0
arieles08 <leandromorinigo@gmail.com>
2022-12-22 02:00:47 -0300

    Properati_calculo_breakdown

--- TP/Properati_BreakDown_apertura_Calculos.R
+++ TP/Properati_BreakDown_apertura_Calculos.R
@@ -44,7 +44,7 @@ explain_rf <- DALEX::explain(model = modelo_randomForest,
 variable_ya <- filter(test_data_d, l3 == "Puerto Madero") #filtra el barrio
 
 variable_y<-variable_ya[9,] #selecciona la n° 9 de ese barrio
-str(variable_y)
+
 
 #breakdown
 bd_rf <- predict_parts(explainer = explain_rf,
@@ -53,48 +53,48 @@ bd_rf <- predict_parts(explainer = explain_rf,
 bd_rf
 plot(bd_rf)
 
+
+
 #------------------------------------
-# Replicamos los pasos del BreakDown para johnny_d:
+# Replicamos los pasos del BreakDown para caso 9 de Puerto Madero (Properati):
 data <- explain_rf[["data"]] #21579 registros y 8 cols
 pred <- explain_rf[["y_hat"]]
 
-#0: intercept              -> 0.235
-mean(predict(titanic_rf, data, type = "prob")[,'yes']) #-> 0.2353095
+#0: intercept              -> 204643.23
+mean(predict(modelo_randomForest, data)) #-> 204643.2
 
 
-#1: age = 8:               -> 0.505 = +0.27 + (0.235)
-data$age <- johnny_d[,3]
-mean(predict(titanic_rf, data, type = "prob")[,'yes']) #-> 0.505121
+#1: l3 = Puerto Madero :               -> 177093.3
+data$l3 <- variable_y[,2]
+mean(predict(modelo_randomForest, data)) #-> 177093.3 > 204643.23 + -27549.96
 
 
-#2: class = 1st            -> 0.591 = +0.086 + (0.235 + 0.27)    
-data$class <- johnny_d[,1]
-mean(predict(titanic_rf, data, type = "prob")[,'yes']) #-> 0.5906969
+#2: surface_uncovered = 55              ->  205625.5 > 204643.23 + -27549.96  + 28532.21  
+data$surface_uncovered <- variable_y[,8]
+mean(predict(modelo_randomForest, data)) #-> 205625.5
 
 
-#3: fare = 72              -> 0.544 = -0.046 + (0.235 + 0.27 + 0.086)
-data$fare <- johnny_d[,6]
-mean(predict(titanic_rf, data, type = "prob")[,'yes']) #-> 0.5443561
+#3: surface_covered = 90              -> 227686.8 > 204643.23 + -27549.96  + 28532.21 + 22061.34
+data$surface_covered <- variable_y[,5]
+mean(predict(modelo_randomForest, data)) #-> 227686.8
 
 
-#4: gender = male          -> 0.461 = -0.083 + (0.235 + 0.27 + 0.086 - 0.046)
-data$gender <- johnny_d[,2]
-mean(predict(titanic_rf, data, type = "prob")[,'yes']) #-> 0.4611518
+#4: bathrooms = 2            -> 248816.3 > 204643.23 + -27549.96  + 28532.21 + 22061.34 + 21129.48
+data$bathrooms <- variable_y[,4]
+mean(predict(modelo_randomForest, data)) #-> 248816.3
 
 
-#5: embarked = Southampton -> 0.458 = -0.003 + (0.235 + 0.27 + 0.086 - 0.046 - 0.083)
-data$embarked <- johnny_d[,7]
-mean(predict(titanic_rf, data, type = "prob")[,'yes']) #-> 0.4584422
+#5: property_type = Departamento -> 254239.4 > 204643.23 + -27549.96  + 28532.21 + 22061.34 + 21129.48 + 5423.07
+data$property_type <- variable_y[,7]
+mean(predict(modelo_randomForest, data)) #-> 254239.4
 
 
-#6: sibsp = 0              -> 0.452 = -0.006 + (0.235 + 0.27 + 0.086 - 0.046 - 0.083 - 0.003)
-data$sibsp <- johnny_d[,4]
-mean(predict(titanic_rf, data, type = "prob")[,'yes']) #-> 0.4523398
+#6: rooms = 3              -> 240551.4 > 204643.23 + -27549.96  + 28532.21 + 22061.34 + 21129.48 + 5423.07  + -13687.97
+data$rooms <- variable_y[,3]
+mean(predict(modelo_randomForest, data)) #-> 240551.4 --> LLEGAMOS AL VALOR DE LA PREDICCION
 
-
-#6: parch = 0              -> 0.422 = -0.03 + (0.235 + 0.27 + 0.086 - 0.046 - 0.083 - 0.003 - 0.006)
-data$parch <- johnny_d[,5]
-mean(predict(titanic_rf, data, type = "prob")[,'yes']) #-> 0.422 --> LLEGAMOS AL VALOR DE LA PREDICCION
+#verificación:
+predict(modelo_randomForest, variable_y) #-> 240551.4 --> VALOR PREDICCION COINCIDE
 
 #----------------------------------
 
