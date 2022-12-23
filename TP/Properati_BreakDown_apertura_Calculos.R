048e6a3c218375b2ea921bb66e9b7b37708c2aac
arieles08 <leandromorinigo@gmail.com>
2022-12-22 21:40:06 -0300

    update

--- TP/Properati_BreakDown_apertura_Calculos.R
+++ TP/Properati_BreakDown_apertura_Calculos.R
@@ -6,6 +6,12 @@ install.packages("stringr")
 library(fastDummies)
 require(data.table)
 library("stringr") 
+library("dplyr")
+library("rsample")
+library(randomForest)
+library(DALEX)
+library(iBreakDown)
+#library(tidyverse)
 
 #carga:
 datos_properati <- read.csv("properati_preprocesado_2022.csv")
@@ -27,7 +33,7 @@ train_test_d <- initial_split(datos_properati_d, prop = 0.75)
 train_data_d <- training(train_test_d)
 test_data_d <- testing(train_test_d)
 
-
+dim(train_data_d)
 #rf
 #opcion: modelo_randomForest <- randomForest(price ~.-id -l3 -precio_en_miles -surface_total, data = train_data_d)
 modelo_randomForest <- randomForest(price ~.-id, data = train_data_d)
@@ -39,12 +45,10 @@ explain_rf <- DALEX::explain(model = modelo_randomForest,
                              label = "Random Forest")
 
 #selecciono observacion
-#variables_test <- test_data_d %>% select(-c(l3, surface_total, price, precio_en_miles))
 
 variable_ya <- filter(test_data_d, l3 == "Puerto Madero") #filtra el barrio
 
-variable_y<-variable_ya[9,] #selecciona la n° 9 de ese barrio
-
+variable_y<-variable_ya[9,] #selecciona la n° 9 de ese barrio (saca id y price para prediccion)
 
 #breakdown
 bd_rf <- predict_parts(explainer = explain_rf,
@@ -53,7 +57,7 @@ bd_rf <- predict_parts(explainer = explain_rf,
 bd_rf
 plot(bd_rf)
 
-
+rownames = TRUE
 
 #------------------------------------
 # Replicamos los pasos del BreakDown para caso 9 de Puerto Madero (Properati):
@@ -96,9 +100,69 @@ mean(predict(modelo_randomForest, data)) #-> 240551.4 --> LLEGAMOS AL VALOR DE L
 #verificación:
 predict(modelo_randomForest, variable_y) #-> 240551.4 --> VALOR PREDICCION COINCIDE
 
-#----------------------------------
+#--------------------------------------------------------------------
+# Entendiendo la interpretación de las predicciones que usa para las contribuciones
+dt <- explain_rf[["data"]]
+dt <- data.table(dt)
+data <- explain_rf[["data"]] #21579 registros y 8 cols
 
 
+#0: intercept              -> 204643.23
+mean(predict(modelo_randomForest, data)) #-> 204643.2
+
+dt[,pred0 := predict(modelo_randomForest, data)]
+
+#1: l3 = Puerto Madero :               -> 177093.3
+data$l3 <- variable_y[,2]
+mean(predict(modelo_randomForest, data)) #-> 177093.3 > 204643.23 + -27549.96
+
+dt[,l3_2 := variable_y[,2]]
+dt[,pred1 := predict(modelo_randomForest, data)]
+
+#2: surface_uncovered = 55              ->  205625.5 > 204643.23 + -27549.96  + 28532.21  
+data$surface_uncovered <- variable_y[,8]
+mean(predict(modelo_randomForest, data)) #-> 205625.5
+
+dt[,surface_uncovered_2 := variable_y[,8]]
+dt[,pred2 := predict(modelo_randomForest, data)]
+
+#3: surface_covered = 90              -> 227686.8 > 204643.23 + -27549.96  + 28532.21 + 22061.34
+data$surface_covered <- variable_y[,5]
+mean(predict(modelo_randomForest, data)) #-> 227686.8
+
+dt[,surface_covered_2 := variable_y[,5]]
+dt[,pred3 := predict(modelo_randomForest, data)]
+
+#4: bathrooms = 2            -> 248816.3 > 204643.23 + -27549.96  + 28532.21 + 22061.34 + 21129.48
+data$bathrooms <- variable_y[,4]
+mean(predict(modelo_randomForest, data)) #-> 248816.3
+
+dt[,bathrooms_2 := variable_y[,4]]
+dt[,pred4 := predict(modelo_randomForest, data)]
+
+#5: property_type = Departamento -> 254239.4 > 204643.23 + -27549.96  + 28532.21 + 22061.34 + 21129.48 + 5423.07
+data$property_type <- variable_y[,7]
+mean(predict(modelo_randomForest, data)) #-> 254239.4
+
+dt[,property_type_2 := variable_y[,7]]
+dt[,pred5 := predict(modelo_randomForest, data)]
+
+#6: rooms = 3              -> 240551.4 > 204643.23 + -27549.96  + 28532.21 + 22061.34 + 21129.48 + 5423.07  + -13687.97
+data$rooms <- variable_y[,3]
+mean(predict(modelo_randomForest, data)) #-> 240551.4 --> LLEGAMOS AL VALOR DE LA PREDICCION
+
+dt[,rooms_2 := variable_y[,3]]
+dt[,pred6 := predict(modelo_randomForest, data)]
+
+#verificación:
+predict(modelo_randomForest, variable_y) #-> 240551.4 --> VALOR PREDICCION COINCIDE
+
+#exporto archivo:
+fwrite(dt, file= "BD_ej9_PuertoMadero_step-by-step.csv", sep = ",")
+
+getwd()
+
+#--------------------------------------------------------------------
 #ibreakdown
 ibd_rf <- predict_parts(explainer = explain_rf,
                         new_observation = variable_y,
